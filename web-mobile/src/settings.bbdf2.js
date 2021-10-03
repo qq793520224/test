375bf0f9e285acaa47b859ba2379400f1c237364
qq793520224 <50006288+qq793520224@users.noreply.github.com>
2021-10-03 18:07:32 +0800

    Add files via upload

---
+++ web-mobile/src/settings.bbdf2.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/01_shouYe/changJing_shouYe.fire",orientation:"",jsList:[],bundleVers:{internal:"1b082",main:"7aa57"}};
